b9c65912c5b52694f81bf1f523a77e084aac0e26
David Escudero <david89964e@gmail.com>
2023-08-14 13:34:44 -0300

    adivina el numero

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 
-function flipCoin() {
-    var resultElement = document.getElementById("result");
-    // Generar un número aleatorio entre 0 (incluido) y 1 (excluido)
-    var flipResult = Math.random();
+
+function adivinaElNumero() {
+    var numeroSecreto = Math.floor(Math.random() * 100) + 1;  // Número secreto entre 1 y 100
+    var intentos = 5;  // El usuario tiene 5 intentos para adivinar el número
     
-    if (flipResult < 0.5) {
-        resultElement.innerText = "Resultado: Cara";
-    } else {
-        resultElement.innerText = "Resultado: Cruz";
+    for(var i = 0; i < intentos; i++) {
+        var numeroUsuario = prompt("Introduce un número entre 1 y 100, tienes " + (intentos - i) + " intentos restantes.");
+        
+        if (numeroUsuario == numeroSecreto) {
+            alert("¡Has acertado el número secreto! Es " + numeroSecreto);
+            return;
+        } else if (numeroUsuario < numeroSecreto) {
+            alert("Tu número es demasiado bajo. Intenta de nuevo.");
+        } else if (numeroUsuario > numeroSecreto) {
+            alert("Tu número es demasiado alto. Intenta de nuevo.");
+        }
     }
+    
+    alert("Lo siento, no has adivinado el número secreto. El número secreto era " + numeroSecreto);
 }
+adivinaElNumero();
+
+
+
